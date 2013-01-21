ded98dad9508a34412113b568f590c96e635c0b4
Jon <jon@onwebcentric.com>
2013-01-21 10:21:00 -0800

    Better Commenting Includes better commenting in jm-ipad.php

--- themes/abundance/jm-ipad.php
+++ themes/abundance/jm-ipad.php
@@ -5,6 +5,12 @@
  * @author Jonathon McDonald <jon@onecentric.com>
  */
 
+/**
+ * Appends a message to the cart that the user should remember
+ * to pick up a rack mount if purchasing a ipad enclosure
+ *
+ * @return String The modified cart message
+ */
 function jm_add_to_cart($message) 
 {
 	global $woocommerce, $product; 
@@ -14,16 +20,26 @@ function jm_add_to_cart($message)
 	return $message;
 }
 
+/**
+ * Listens to see if an item is added to the cart.  If it is,
+ * this will check to see if the item is a ipad enclosure.  
+ * If the item is an ipad enclosure it will then add a filter
+ * telling the customer to pick up an ipad enclosure
+ */
 function jm_add_to_cart_listener( $cart_item_key, $product_id, $quantity, $variation_id, $variation, $cart_item_data )
 {
+	// Fetch a product object using the product id
 	$product = new WC_Product( $product_id );
 
+	// Get all of the product categories this item belongs too
 	$terms = wp_get_post_terms( $product->get_post_data()->ID, 'product_cat' );
 
+	// Add all the categories to an array
 	foreach( $terms as $term ) {
 		$categories[] = $term->slug;
 	}
 
+	// If the array contains ipad-enclosures we know this was indeed a enclosure
 	if( in_array('ipad-enclosures', $categories) ) {
 		add_filter('woocommerce_add_to_cart_message', 'jm_add_to_cart', 999);
 	}
